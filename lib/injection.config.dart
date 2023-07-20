b61d420ba7dfb22d50eec5af7aaa3ae42073c6ba
Dani Roman <daniel.roman@thehiddenpanda.com>
2023-07-20 17:26:26 +0200

    VBQ-5: Fix UT + More UT about Splash Screen --> Navigate to Home

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -12,17 +12,14 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import 'application/bloc/question/question_bloc.dart' as _i7;
-import 'application/bloc/quiz_screen/quiz_screen_bloc.dart' as _i12;
-import 'application/bloc/splash_screen/splash_screen_bloc.dart' as _i13;
-import 'domain/repositories/question_api_repository.dart' as _i5;
-import 'domain/repositories/quiz_repository.dart' as _i8;
+import 'application/bloc/question/question_bloc.dart' as _i5;
+import 'application/bloc/quiz_screen/quiz_screen_bloc.dart' as _i10;
+import 'application/bloc/splash_screen/splash_screen_bloc.dart' as _i11;
+import 'domain/repositories/quiz_repository.dart' as _i6;
 import 'domain/services/http_service.dart' as _i3;
-import 'domain/services/ui/quiz_service.dart' as _i10;
-import 'domain/services/ui/splash_service.dart' as _i11;
-import 'infrastructure/repositories/question/http_question_repository.dart'
-    as _i6;
-import 'infrastructure/repositories/quiz/mock_quiz_repository.dart' as _i9;
+import 'domain/services/ui/quiz_service.dart' as _i8;
+import 'domain/services/ui/splash_service.dart' as _i9;
+import 'infrastructure/repositories/quiz/mock_quiz_repository.dart' as _i7;
 import 'infrastructure/services/http_service.dart' as _i4;
 
 // ignore_for_file: unnecessary_lambdas
@@ -39,18 +36,15 @@ _i1.GetIt $initGetIt(
     environmentFilter,
   );
   gh.factory<_i3.HttpService>(() => _i4.FlutterHttpService());
-  gh.factory<_i5.QuestionApiRepository>(
-      () => _i6.HttpQuestionRepository(gh<_i3.HttpService>()));
-  gh.lazySingleton<_i7.QuestionBloc>(() => _i7.QuestionBloc());
-  gh.factory<_i8.QuizRepository>(() => _i9.MockQuizRepository());
-  gh.factory<_i10.QuizService>(
-      () => _i10.QuizService(gh<_i8.QuizRepository>()));
-  gh.factory<_i11.SplashService>(() => _i11.SplashService());
-  gh.lazySingleton<_i12.QuizScreenBloc>(() => _i12.QuizScreenBloc(
-        gh<_i10.QuizService>(),
-        gh<_i7.QuestionBloc>(),
+  gh.lazySingleton<_i5.QuestionBloc>(() => _i5.QuestionBloc());
+  gh.factory<_i6.QuizRepository>(() => _i7.MockQuizRepository());
+  gh.factory<_i8.QuizService>(() => _i8.QuizService(gh<_i6.QuizRepository>()));
+  gh.factory<_i9.SplashService>(() => _i9.SplashService());
+  gh.lazySingleton<_i10.QuizScreenBloc>(() => _i10.QuizScreenBloc(
+        gh<_i8.QuizService>(),
+        gh<_i5.QuestionBloc>(),
       ));
-  gh.lazySingleton<_i13.SplashScreenBloc>(
-      () => _i13.SplashScreenBloc(gh<_i11.SplashService>()));
+  gh.lazySingleton<_i11.SplashScreenBloc>(
+      () => _i11.SplashScreenBloc(gh<_i9.SplashService>()));
   return getIt;
 }
--- test/ui/screens/onboarding_screen/onboarding_screen_test.dart
+++ test/ui/screens/onboarding_screen/onboarding_screen_test.dart
@@ -1,10 +1,39 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:verbaquiz/application/bloc/splash_screen/splash_screen_bloc.dart';
+import 'package:verbaquiz/ui/screens/home_screen/home_screen.dart';
 import 'package:verbaquiz/ui/screens/splash_screen/splash_screen.dart';
 
+import 'onboarding_screen_test.mocks.dart';
+
+@GenerateMocks([SplashScreenBloc])
+
 void main() {
+  late SplashScreenBloc splashScreenBloc;
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() {
+    splashScreenBloc = MockSplashScreenBloc();
+
+    final getIt = GetIt.instance;
+    getIt.registerSingleton<SplashScreenBloc>(splashScreenBloc);
+  });
+
   group('Splash Screen Test', () {
-    testWidgets('Pump and test widget', (WidgetTester tester) async {
+    testWidgets('Pump and test widget - Initial', (WidgetTester tester) async {
+
+      const SplashScreenState state = SplashScreenInitial();
+
+      when(splashScreenBloc.stream).thenAnswer((_){
+        when(splashScreenBloc.state).thenAnswer((_) => state);
+        return Stream.value(state);
+      });
 
       await tester.pumpWidget(
         const MaterialApp(
@@ -22,7 +51,52 @@ void main() {
 
       await expectLater(
         find.byType(MaterialApp),
-        matchesGoldenFile('../../goldens/splash_screen.png'),
+        matchesGoldenFile('../../goldens/splash_screen_1.png'),
+      );
+    });
+
+    testWidgets('Pump and test widget - Goes to Home', (WidgetTester tester) async {
+
+      const SplashScreenState state = SplashScreenGotNextPage(nextPage: HomeScreen.routeName);
+
+      when(splashScreenBloc.stream).thenAnswer((_){
+        when(splashScreenBloc.state).thenAnswer((_) => state);
+        return Stream.value(state);
+      });
+
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: SplashScreen(),
+        ),
+      );
+
+      expect(find.image(const AssetImage('assets/splash_screen/verbaquiz_logo.png')), findsOneWidget);
+
+      Finder textFinder = find.text('TheHiddenPanda');
+      expect(textFinder, findsOneWidget);
+
+      Text text = tester.firstWidget(textFinder);
+      expect(text.style?.fontFamily, 'KuaiLe');
+
+      await expectLater(
+        find.byType(MaterialApp),
+        matchesGoldenFile('../../goldens/splash_screen_2.png'),
+      );
+
+      await tester.pumpAndSettle();
+
+      textFinder = find.text('Home');
+      expect(textFinder, findsOneWidget);
+
+      textFinder = find.text('Quiz con timer');
+      expect(textFinder, findsOneWidget);
+
+      textFinder = find.text('Quiz sin timer');
+      expect(textFinder, findsOneWidget);
+
+      await expectLater(
+        find.byType(MaterialApp),
+        matchesGoldenFile('../../goldens/splash_screen_2.png'),
       );
     });
   });
